a3ecad25d2dfa989d1308fd2c8a6f9188aa8dc2d
Mickaël Schoentgen <contact@tiger-222.fr>
2026-02-19 22:58:44 +0100

    sync: X-Men

--- examples/x-men.py
+++ examples/x-men.py
@@ -14,14 +14,15 @@ def xmen() -> None:
     """
 
     events = {
-        2005: ("X-Men", "X2"),
-        2006: "X-Men: The Last Stand",
-        1990: "X-Men Origins: Wolverine",
+        2000: ("X-Men", "X2"),
+        2001: "X-Men: The Last Stand",
+        1980: "X-Men Origins: Wolverine",
         1962: "X-Men: First Class",
         2013: "The Wolverine",
-        2023: "X-Men: Days of Future Past",
+        1973: "X-Men: Days of Future Past",
         1983: "X-Men: Apocalypse",
         2029: "Logan",
+        1992: "Dark Phoenix",
     }
     args = {
         "padding": 3,
